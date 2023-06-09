bd0f142e8bb9a5f313c98b07b592cb609cdf9f79
Antones N. Lima <anl@ic.ufal.br>
2023-06-09 13:49:11 -0300

    calculo do erro médio finalizado

--- CalcularErroMedio.v3.py
+++ CalcularErroMedio.v3.py
@@ -11,6 +11,16 @@ pasta_originais = "HuGaDB_RFOnly"
 # Pasta com os dados filtrados
 pasta_filtrados = "HuGaDB_RFOnly/Complementar"
 
+# Outras pastas e suas respectivas colunas
+pastas = [
+    ("HuGaDB_RF_CalibratedAccGyro", "_Calibrated"),
+    ("HuGaDB_RF_CalibratedAccGyro/Complementar", "_Calibrated_Complementar"),
+    ("Single/Kalman", "_Kalman"),
+    ("Single/Kalman/Complementar", "_Kalman_Complementar"),
+    ("Single/LowPass", "_LowPass"),
+    ("Single/LowPass/Complementar", "_LowPass_Complementar")
+]
+
 # Pasta para salvar o arquivo de resultados
 pasta_analises = "Análises"
 
@@ -32,16 +42,26 @@ for arquivo in arquivos:
         dados_originais = pd.read_excel(caminho_originais)
         
         # Leitura dos dados filtrados
-        dados_filtrados = pd.read_excel(caminho_filtrados[:-5]+"_complementar.xlsx")
+        dados_filtrados = pd.read_excel(caminho_filtrados)
         
         # Seleção das colunas dos eixos do acelerômetro
         colunas = ["rfax", "rfay", "rfaz"]
         
         # Cálculo do erro médio para cada coluna
         for coluna in colunas:
-            erro_medio = calcular_erro_medio(dados_filtrados[coluna[:-2]+coluna[-1:]], dados_originais[coluna])
+            erro_medio = calcular_erro_medio(dados_filtrados[coluna], dados_originais[coluna])
             nome_coluna_resultado = coluna + "_complementar"
             df_erros_medios.loc[arquivo, nome_coluna_resultado] = erro_medio
+            
+        # Cálculo do erro médio para as outras pastas
+        for pasta, sufixo in pastas:
+            caminho_filtrados_outros = os.path.join(pasta, arquivo)
+            dados_filtrados_outros = pd.read_excel(caminho_filtrados_outros)
+            
+            for coluna in colunas:
+                erro_medio = calcular_erro_medio(dados_filtrados_outros[coluna], dados_originais[coluna])
+                nome_coluna_resultado = coluna + sufixo
+                df_erros_medios.loc[arquivo, nome_coluna_resultado] = erro_medio
 
 # Caminho completo para o arquivo de resultados
 caminho_resultados = os.path.join(pasta_analises, nome_arquivo_resultados)
